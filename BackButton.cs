5e4b0a13036cad58248958420e1ae0477e4ebd54
Tanay Gupta <tanaygupta.gupta@gmail.com>
2018-06-25 09:16:59 +0530

    Add files via upload

---
+++ BackButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+ 
+public class BackButton : MonoBehaviour
+{
+
+	int sceneIndex;	
+	 void start()
+	 {
+		 sceneIndex=SceneManager.GetActiveScene().buidIndex;
+	 }
+	 
+	 void Update()
+	 {
+		 if(Input.GetKeyDown(Keycode.Escape))
+		 {
+			 //go out of the application
+			 if(sceneIndex>0)
+			 {
+				 SceneManager.LoadScene(sceneIndex-1);
+			 }
+			 else
+			 {
+				 Application.Quit();
+			 }
+			 
+		 }
+		 
+	 }
+	// for loading new scenes you can call this function in another classes
+	public void LoadNextScene()
+	 {
+		SceneManager.LoadScene(sceneIndex+1);
+	 }
+	 
+}
